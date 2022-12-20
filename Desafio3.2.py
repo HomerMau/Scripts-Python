2690f33841484395d8c644a4609aa3bab3ed14c2
HomerMau <titi020604@gmail.com>
2022-12-19 22:34:03 -0300

    2

---
+++ Desafio3.2.py
@@ -0,0 +1,6 @@
+print('====== DESAFIO 03.2 ======')
+num1 = int(input ('Primeiro Numero '))
+num2 = int(input ('Segundo Numero '))
+s = num1 + num2
+print('A soma vale', s)
+print('A soma vale {}'.format(s))
